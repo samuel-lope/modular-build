94b2856b3169f995afd844bd88351b76a02c4bc9
Samuel Lopes <68786643+samuel-lope@users.noreply.github.com>
2025-10-03 18:52:03 -0300

    change worker.js to null

--- src/worker.js
+++ src/worker.js
@@ -9,10 +9,10 @@
  * primeira versão estável
  */
 
-export default {
-  async fetch(request, env, ctx) {
-    // You can view your logs in the Observability dashboard
-    console.info({ message: 'Hello World Worker received a request!' });
-    return new Response('Hello World!');
-  }
-};
+// export default {
+//   async fetch(request, env, ctx) {
+//     // You can view your logs in the Observability dashboard
+//     console.info({ message: 'Hello World Worker received a request!' });
+//     return new Response('Hello World!');
+//   }
+// };
